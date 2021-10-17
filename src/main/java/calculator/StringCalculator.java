39795a38e083eab82a5b3e040a062103e0ed8708
Priya Patil <padmapriyamalgave@gmail.com>
2021-10-17 20:37:47 +0530

    Test case to validate add() method with input as empty String

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,60 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
+	 public int add(String input) {
+	    	if(input.equals("")){
+				return 0;
+			}
+			else{
+				String delimiter = ",";
+				if(input.matches("//(.*)\n(.*)")){
+					delimiter = Character.toString(input.charAt(2));
+					input = input.substring(4);
+				}
+				
+				String numList[] = splitNumbers(input, delimiter + "|\n");
+				return sum(numList);
+			}
+		}
+    
+    private boolean isEmpty(String input) {
+		return input.isEmpty() ;
+    	
     }
+    
+    private static int stringtoInt(String input) {
+    	return Integer.parseInt(input);
+    	
+    }
+    
+    private static String[] splitNumbers(String numbers, String divider){
+		//System.out.println(divider);
+	    return numbers.split(divider);
+	}
+    
+    
+    
+    private static int sum(String[] numbers){
+ 	    int total = 0;
+ 	    String negString = "";
+
+        for(String number : numbers){
+        	if(stringtoInt(number) < 0){
+        		if(negString.equals(""))
+        			negString = number;
+        		else
+        			negString += ("," + number);
+        	}
+        	if(stringtoInt(number) < 1000)
+		    	total += stringtoInt(number);
+		}
+        if(!negString.equals("")){
+			throw new IllegalArgumentException("Negatives not allowed: " + negString);
+		}
+
+		return total;
+    }
+    
+    
 
 }
